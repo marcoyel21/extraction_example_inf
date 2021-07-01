f4bb274a56537d3cac5656bb31a3f5dfd5871a85
Marco Ramos <57338890+marcoyel21@users.noreply.github.com>
2021-06-30 21:17:19 -0500

    Correccion de matriz

--- particular_cleaning.R
+++ particular_cleaning.R
@@ -22,11 +22,11 @@ horario<-matrix(horario$V1,ncol = 1, nrow= docs)%>% as.data.frame()%>% rename(ho
 plazo_min<-matrix(plazo_min$V1,ncol = 1, nrow= docs)%>% as.data.frame()%>% rename(plazo_min=V1)
 
 #para matrices 
-fondo<-t(matrix(fondo$V1,ncol = 5, nrow= docs)) %>% as.data.frame() %>% 
+fondo<-t(matrix(fondo$V1,ncol = docs, nrow= 5)) %>% as.data.frame() %>% 
   rename(nombre=V1, sociedad_y_razon=V2, tipo=V3, clase= V4,plazo_categ=V5)
-ren_bruto<-t(matrix(ren_bruto$V1,ncol = 5, nrow= docs))%>% as.data.frame() %>%
+ren_bruto<-t(matrix(ren_bruto$V1,ncol = docs, nrow= 5))%>% as.data.frame() %>%
   rename(rend_anual_brut_ultmes=V1, rend_anual_brut_ult3mes=V2, rend_anual_brut_ult12mes=V3, rend_anual_brut_min= V4,rend_anual_brut_max=V5)
-ren_neto<-t(matrix(ren_neto$V1,ncol = 5, nrow= docs))%>% as.data.frame() %>%
+ren_neto<-t(matrix(ren_neto$V1,ncol = docs, nrow= 5))%>% as.data.frame() %>%
   rename(rend_anual_net_ultmes=V1, rend_anual_net_ult3mes=V2, rend_anual_net_ult12mes=V3, rend_anual_net_min= V4,rend_anual_net_max=V5)
 
 
